895dc88be642b1b3c95b4917730bff3b91245a3e
neuroflip <jaanguita@gmail.com>
2025-12-23 19:17:38 +0100

    fix: After move test directory, corrected paths

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    setupFiles: ["./test/setupTests.js"]
+    setupFiles: ["./src/test/setupTests.js"]
   },
 })
