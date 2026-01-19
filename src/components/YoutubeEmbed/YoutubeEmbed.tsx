6d51e4e8b37c299a5f6c62d54edb3c1511b2770a
neuroflip <jaanguita@gmail.com>
2026-01-19 17:27:19 +0100

    tests: added tests for youtube embed component

--- src/components/YoutubeEmbed/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed/YoutubeEmbed.tsx
@@ -1,18 +1,8 @@
+import { getVideoIdFromYoutubeUrl } from './helpers/utils';
 import type { YoutubeEmbedProps } from './YoutubeEmbed.types';
-import { YOTUBE_PATTERN } from '../../helpers/utils';
-
-const getVideoId = (url: string) => {
-  const match = url.match(YOTUBE_PATTERN);
-
-  if (match && match[3].length === 11) {
-      return match[3];
-  } else {
-      return;
-  }
-}
 
 function YoutubeEmbed({ url, className }: YoutubeEmbedProps) {
-    const videoId = getVideoId(url)
+    const videoId = getVideoIdFromYoutubeUrl(url)
 
     if (videoId) {
       return <div className={ className ? className : '' }>
