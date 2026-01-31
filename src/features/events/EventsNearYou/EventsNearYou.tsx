e99aa22155577e503e9c9b5dde72078865400e48
neuroflip <jaanguita@gmail.com>
2026-01-31 13:23:17 +0100

    refactor: Events near you hook with logic

--- src/features/events/EventsNearYou/EventsNearYou.tsx
+++ src/features/events/EventsNearYou/EventsNearYou.tsx
@@ -1,64 +1,13 @@
 import Spinner from "@/components/ui/Spinner/Spinner";
-import { searchEventsByGeo } from "@/database/eventsRpc";
-import { getGeolocationPosition } from "@/features/maps/GpxRouteMap/helpers/mapUtils";
 import type { RouteEvent } from "@/types/RouteEvent.types";
-import L from "leaflet";
-import React from "react";
-import toast from "react-hot-toast";
 import RouteEventCard from "../RouteEventCard/RouteEventCard";
 import ItemCardList from "@/components/ItemCardList/ItemCardList";
 import { useTranslation } from "react-i18next";
+import useEventsNearYou from "./hooks/useEventsNearYou";
 
 const EventsNearYou = () => {
   const { t } = useTranslation("myEvents");
-  const [ location, setLocation ] = React.useState<GeolocationPosition | null>(null);
-  const [ isLoading, setIsLoading ] = React.useState(false);
-  const [ routeEvents, setRouteEvents ] = React.useState<Array<RouteEvent>>([]);
-  
-  React.useEffect(() => {
-    setIsLoading(true);
-
-    getGeolocationPosition((data) => {
-      setLocation(data);
-      setIsLoading(false);
-    }, () => {
-      setIsLoading(false);
-      throw new Error('cannot geolocate your position');
-    });
-  }, []);
-
-  React.useEffect(() => {
-    const searchLocalEvents = async () => {
-      if (location) {
-        setIsLoading(true);
-        const lat = location?.coords.latitude;
-        const lon = location?.coords.longitude;
-        const southWest = new L.LatLng(lat - 0.1, lon - 0.1);
-        const northEast = new L.LatLng(lat + 0.1, lon + 0.1);
-        const bounds = new L.LatLngBounds(southWest, northEast);
-        const response = await searchEventsByGeo(bounds);
-
-        if (!response.error) {
-          setIsLoading(false);
-          return response.data;
-        } else {
-          setIsLoading(false);
-          throw new Error(`Error searching routes: ${response.error.message}`);
-        }
-      }
-    }
-
-    if (location) {
-      const promise = searchLocalEvents()
-      
-      promise.then((data) => {
-        setRouteEvents(data);
-      }).catch((e: unknown) => {
-          toast.error((e as Error).message);
-      });
-    }
-  }, [location])
-
+  const [ isLoading, routeEvents ] = useEventsNearYou();
   const routeEventCard = (event: RouteEvent) => 
     <RouteEventCard key={ event.id } routeEvent={ event } />;
 
