13d385d887e7ba4d5f492c0324a6e82cf7615858
neuroflip <jaanguita@gmail.com>
2026-02-05 18:03:39 +0100

    feat: offline detection and message in main layout for pages

---
+++ src/components/Offline/Offline.tsx
@@ -0,0 +1,16 @@
+import { isOffline } from "@/helpers/offline";
+import { useTranslation } from "react-i18next";
+
+const Offline = () => {
+  const { t } = useTranslation(["landing"])
+
+  if (isOffline()) {
+    return <div className="flex items-center w-full bg-primary text-white text-xl h-25 justify-center">
+      ⚠️ { t("main.offline") } ⚠️
+    </div>
+  } else {
+    return <></>
+  }
+}
+
+export default Offline;
---
+++ src/components/Offline/helpers/offline.ts
@@ -0,0 +1,3 @@
+const isOffline  = () => !navigator.onLine;
+
+export { isOffline };
